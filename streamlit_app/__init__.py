100a25ca8f5de710da4f81c5622660a55493a322
shaoniwang <50398673+shaoni-wang@users.noreply.github.com>
2026-07-20 21:55:40 +0200

    Delete streamlit_app directory

--- streamlit_app/__init__.py
+++
@@ -1,8 +0,0 @@
-# streamlit_app/__init__.py
-# Makes the streamlit_app directory a Python package
-
-# This file can be empty, but it's needed to make the directory a package
-# It allows imports from the streamlit_app directory
-
-# Optional: Add version info
-__version__ = "0.1.0"
